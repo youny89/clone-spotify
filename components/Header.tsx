cd2451488011655d0c4ccf4591cc7e806ffc8250
youny89 <youny@kakao.com>
2023-06-16 17:51:57 +0900

    logout

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,12 @@ import { HiHome } from "react-icons/hi"
 import { BiSearch } from "react-icons/bi"
 import Button from "./Button"
 import useAuthModal from "@/hooks/useAuthModal"
+import { 
+    useSupabaseClient,
+ } from "@supabase/auth-helpers-react"
+import { useUser } from "@/hooks/useUser"
+import { FaUserAlt } from "react-icons/fa"
+import { toast } from "react-hot-toast"
 
 interface HeaderPops {
     children: React.ReactNode
@@ -16,7 +22,22 @@ interface HeaderPops {
 const Header = ({ children, className }: HeaderPops) => {
     const router = useRouter();
     const authModal = useAuthModal();
-    const handleLogout = () => {}
+    const supabaseClient = useSupabaseClient();
+    const {user, subscription} = useUser();
+
+    const handleLogout = async () => {
+        const { error } = await supabaseClient.auth.signOut()
+
+        // TODO: reset any playing songs
+
+        router.refresh()
+
+        if(error) {
+            toast.error(error.message)
+        } else {
+            toast.success('logged out!')
+        }
+    }
 
     return (
         <div
@@ -94,7 +115,17 @@ const Header = ({ children, className }: HeaderPops) => {
                         items-center
                         gap-x-4
                     "
-                >
+                >   
+                { user ? (
+                    <div className="flex gap-x-4 items-center">
+                        <Button onClick={handleLogout} className="bg-white px-6 py-2">Logout</Button>
+                        <Button onClick={()=> router.push('/account')} className="bg-white">
+                            <FaUserAlt/>
+                        </Button>
+
+                    </div>
+                ):(
+
                     <>
                         <div>
                             <Button
@@ -121,8 +152,10 @@ const Header = ({ children, className }: HeaderPops) => {
                             </Button>
                         </div>
                     </>
+                )}
                 </div>
             </div>
+
             <div>{children}</div>
         </div>
     )
---
+++ providers/ToastProvider.tsx
@@ -0,0 +1,13 @@
+import { Toaster } from "react-hot-toast"
+
+const ToastProvider = () => {
+    return (
+        <Toaster
+            toastOptions={{
+                style: { background:'#333', color:'#fff'}
+            }}
+        />
+    )
+}
+
+export default ToastProvider
